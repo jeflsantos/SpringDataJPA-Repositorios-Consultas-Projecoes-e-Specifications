229f2b779da8fae5104e909356cedac77e77bb60
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-25 10:53:47 -0300

    Spring Data JPA: Repositórios, Consultas, Projeções e Specifications Spring Data JPA: Repositórios, Consultas, Projeções e Specifications
    Atualizando o registro

--- spring-data/src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ spring-data/src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,7 +9,8 @@ import br.com.alura.spring.data.repository.CargoRepository;
 
 @Service
 public class CrudCargoService {
-
+	
+	private Boolean system = true;
 	private final CargoRepository cargoRepository;
 
 	public CrudCargoService(CargoRepository cargoRepository) {
@@ -17,7 +18,30 @@ public class CrudCargoService {
 	}
 	
 	public void inicial(Scanner scanner) {
-		salvar(scanner);
+		
+		while(system) {
+			System.out.println("Qual ação de cargo deseja executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Salvar");
+			System.out.println("2 - Atualizar");
+			
+			int action = scanner.nextInt();
+			
+			switch (action) {
+			case 1:
+				salvar(scanner);
+				break;
+			case 2:
+				atualizar(scanner);
+				break;
+
+			default:
+				system = false;
+				break;
+			}
+		}
+		
+		
 	}
 	
 	private void salvar(Scanner scanner) {
@@ -27,5 +51,17 @@ public class CrudCargoService {
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
 		System.out.println("Salvo");
+	}
+	
+	private void atualizar(Scanner scanner) {
+		System.out.println("Id");
+		int id = scanner.nextInt();
+		System.out.println("Descrição do Cargo");
+		String descricao = scanner.next();
+		Cargo cargo = new Cargo();
+		cargo.setId(id);
+		cargo.setDescricao(descricao);
+		cargoRepository.save(cargo);
+		System.out.println("Atualizado");
 	}
 }
